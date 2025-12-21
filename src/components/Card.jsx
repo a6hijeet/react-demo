f011a40e12bba1e5d071ae8dede5348c0cee70d7
a6hijeet <abhijeetsatpute.dev@gmail.com>
2025-12-21 15:19:46 +0000

    props and prop types

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,31 @@
 import cardimg from '../assets/card1.jpg'
+import PropTypes from 'prop-types'
 
-function Card() {
+function Card(props) {
 
   return (
     <div className="card-wrapper">
       <img loading="lazy" width={250} height={150} src={cardimg}></img>
-      <div className="card-title">Card title</div>
+      <div className="card-title"><strong>{props.name}</strong></div>
+      <div className="card-desc">{props.desc}</div>
+      <div className="card-date"><i>{props.postDate}</i></div>
+      <div className="card-visibility"><i>{props.visible ? "Published" : "Unpublished"}</i></div>
     </div>
   )
 
 }
+Card.propTypes = {
+  name: PropTypes.string,
+  desc: PropTypes.string,
+  postDate: PropTypes.instanceOf(Date),
+  visible: PropTypes.boolean
 
+}
+
+Card.defaultProps = {
+  name: "Default Post",
+  desc: "Default description",
+  postDate: "Test",
+  visible: false
+}
 export default Card
