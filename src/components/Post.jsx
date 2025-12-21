919e414029253948fd14e160e2a49c28c6419fb3
abhijeet.satpute <abhijeet.satpute@focalworks.in>
2025-12-21 17:26:38 +0000

    Sort and filter data

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,21 +1,19 @@
 import Card from "./Card";
 
 function Post() {
-  const post1 = "Post Title 1";
-  const post2 = "Post Title 2";
-  const post3 = "Post Title 3";
-  const post4 = "Post Title 4";
+
   const postDate = new Date().toLocaleString().split(',')[0];
+  const posts = [
+    {name: "Post Title 1", desc: "Post 1 description", visible:true, postDate:postDate, views: 210},
+    {name: "Post Title 2", desc: "Post 2 description", visible:true, postDate:postDate, views: 120},
+    {name: "Post Title 3", desc: "Post 3 description", visible:true, postDate:postDate, views: 230},
+    {name: "Post Title 4", desc: "Post 4 description", visible:false, postDate:postDate, views: 220},
+    {name: "Post Title 5", desc: "Post 5 description", visible:false, postDate:postDate, views: 420},
+    {name: "Post Title 6", desc: "Post 6 description", visible:false, postDate:postDate, views: 520}
+  ];
+
   return ( 
-   <>
-   <div className="posts-wrapper">
-       <Card name={post1} desc="Sample description" visible={true} postDate={postDate}/>
-       <Card name={post2} desc="Sample description" visible={true} postDate={postDate}/>
-       <Card name={post3} desc="Sample description" visible={false} postDate={postDate}/>
-       <Card name={post4} desc="Sample description" visible={false} postDate={postDate}/>
-       <Card />
-     </div>
-   </>
+       <Card items={posts} category="post"/>
    
   )
 }
