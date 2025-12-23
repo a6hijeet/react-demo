711ecc11dccfad56448cba73e18b42481fa7c007
abhijeet.satpute <abhijeet.satpute@focalworks.in>
2025-12-23 08:05:52 +0000

    Added Navigation using Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,41 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Post from './components/Post'
 import Story from './components/Story'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Color from './components/Color'
 
+
+function Home() {
+  return(
+    <>
+     <Story />
+      <Post />
+    </>
+  )
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Story />
-      <Post />
+      <nav>
+        <ul>
+          <li><Link to="/">Home </Link></li>
+          <li> <Link to="/posts">Posts </Link></li>
+          <li> <Link to="/stories">Stories </Link></li>
+          <li><Link to="/color">Color </Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/posts" element={<Post />}/>
+        <Route path="/stories" element={<Story />}/>
+        <Route path="/color" element={<Color />}/>
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
